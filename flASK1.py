83b2653f0a063eca83e62840658eda66dd1345cf
shreyash1611 <shreya7c@gmail.com>
2023-08-15 17:14:59 +0530

    First Project

---
+++ flASK1.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<p>Welcome to my first small project.</p>"
+
+@app.route("/Aboutme")
+def aboutme():
+    return "<p>My name is Shreyash Chaurasia. I'm currently a 3rd year student at MIT ADTU Pune for Blockchain specialization. I've graduated my high school from Navrachana School, Vadodara in the year 2021.<p>"
+
+@app.route("/interests")
+def interests():
+    return render_template('index.html')
+    
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
